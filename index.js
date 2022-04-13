80b2688f1e35614dacd9db37781c6a3afed19108
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-04-13 10:05:43 +0200

    Add Js for popup modal

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const nav = document.querySelector('.nav-list');
 
 const mobileBtnExit = document.getElementById('mobile-exit');
 
-const modal = document.getElementById("myModal");
+const modal = document.getElementById('myModal');
 
 const btns = document.querySelectorAll('.myBtn');
-
-const span = document.querySelector('.close');
 
 mobileBtn.addEventListener('click', () => {
   nav.classList.add('menu-btn');
@@ -22,18 +20,14 @@ nav.addEventListener('click', () => {
   nav.classList.remove('menu-btn');
 });
 
-[].forEach.call(btns, function(el) {
-  el.onclick = function() {
-    modal.style.display = "block";
-  }
+[].forEach.call(btns, (el) => {
+  el.onclick = function e() {
+    modal.style.display = 'block';
+  };
 });
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = function e(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
+};
--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,15 @@
 let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+let i;
+const slides = document.getElementsByClassName('mySlides');
+const dots = document.getElementsByClassName('demo');
+if (slideIndex > slides.length) { slideIndex = 1; }
+if (slideIndex < 1) { slideIndex = slides.length; }
+for (i = 0; i < slides.length; i += 1) {
+  slides[i].style.display = 'none';
 }
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+for (i = 0; i < dots.length; i += 1) {
+  dots[i].className = dots[i].className.replace(' active', '');
 }
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+slides[slideIndex - 1].style.display = 'block';
+dots[slideIndex - 1].className += ' active';
